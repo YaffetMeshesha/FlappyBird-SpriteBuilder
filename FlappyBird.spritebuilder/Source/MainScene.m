f8a5d164335c9b62049b215fbecab148ffa4e409
Yaffet Meshesha <sora92494@hotmail.com>
2014-11-24 14:32:57 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -82,7 +82,7 @@
 - (void)gameOver {
     if (!_gameOver) {
         _gameOver = TRUE;
-        _restartButton.hidden = TRUE;
+        _restartButton.hidden = FALSE;
         
         character.physicsBody.velocity = ccp(0.0f, character.physicsBody.velocity.y);
         character.rotation = 90.f;
